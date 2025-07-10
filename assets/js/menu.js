8314677e176d87106f7bb08f2d72634de326d946
gupta-suyash <sugupta@ucdavis.edu>
2025-07-10 00:37:49 -0400

    edits

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var menuToggle = document.getElementById("menu-toggle");
-  var nav = document.getElementById("nav");
-  menuToggle.addEventListener("click", function() {
-    nav.classList.toggle("open");
-  });
-});
-
